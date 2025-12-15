b114d5047c438562fd502b00b7c4ace317cac8d8
Toby Cole <misterlab@gmail.com>
2025-12-15 22:23:34 +0100

    Update CONF_DATA range from 150 to 80

--- components/samsung_nasa/nasa/numbers.py
+++ components/samsung_nasa/nasa/numbers.py
@@ -129,7 +129,7 @@ numbers = {
     0x8236: {
         NASA_LABEL: "VAR_OUT_CONTROL_ORDER_CFREQ_COMP1",
         NASA_MODE: CONTROLLER_MODE_CONTROL,
-        CONF_DATA: cmd_numeric_data(0, 150, 1),
+        CONF_DATA: cmd_numeric_data(0, 80, 1),
         CONF_DEFAULTS: number_defaults()
     },
     0x424A: {
